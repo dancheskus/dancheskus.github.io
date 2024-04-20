1ac73e2d953cf3ce99723e38428c66662ea724b4
Daniel Shleifman <dancheskus@gmail.com>
2024-04-20 17:22:53 +0300

    test

--- lampa-remove.js
+++ lampa-remove.js
@@ -1,13 +1,22 @@
 (function () {
-  Lampa.Listener.follow('app', (e) => {
+  Lampa.Listener.follow('app', e => {
     if (e.type !== 'ready') return
 
-    console.log(Lampa)
-    $("[data-action=anime]").eq(0).remove();
-    $("[data-action=timetable]").eq(0).remove();
-    $("[data-action=feed]").eq(0).remove();
-    $("[data-action=myperson]").eq(0).remove();
-    $("[data-action=subscribes]").eq(0).remove();
-    $("[data-action=about]").eq(0).remove();
+    removeItemsFromMenu()
+    test()
   });
 })();
+
+const removeItemsFromMenu = () => {
+  $("[data-action=anime]").eq(0).remove();
+  $("[data-action=timetable]").eq(0).remove();
+  $("[data-action=feed]").eq(0).remove();
+  $("[data-action=myperson]").eq(0).remove();
+  $("[data-action=subscribes]").eq(0).remove();
+  $("[data-action=about]").eq(0).remove();
+}
+
+const test = () => {
+  console.log('bublik')
+  console.log(Lampa)
+}
