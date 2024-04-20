d2e5be2ade15f84b13f4380cf17c7a1d83be654b
Daniel Shleifman <dancheskus@gmail.com>
2024-04-20 16:52:23 +0300

    test

--- lampa-remove.js
+++ lampa-remove.js
@@ -4,6 +4,7 @@
   Lampa.Listener.follow('app', (e) => {
     if (e.type == 'ready') {
       setTimeout(function () {
+        console.log('baranka')
         $("[data-action=anime]").eq(0).remove();
       }, 10);
     }
