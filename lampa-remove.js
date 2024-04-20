b34ca6353ff7b56c3c7d5d859f05ab28ccb28f0a
Daniel Shleifman <dancheskus@gmail.com>
2024-04-21 02:13:35 +0300

    test

--- lampa-remove.js
+++ lampa-remove.js
@@ -271,212 +271,212 @@
   }
 
   function component() {
-    var network = new Lampa.Reguest();
-    var scroll = new Lampa.Scroll({
-      mask: true,
-      over: true
-    });
-    var items = [];
-    var html = $('<div></div>');
-    var active = 0;
+  //   var network = new Lampa.Reguest();
+  //   var scroll = new Lampa.Scroll({
+  //     mask: true,
+  //     over: true
+  //   });
+  //   var items = [];
+  //   var html = $('<div></div>');
+  //   var active = 0;
 
-    this.create = function () {
-      var _this = this;
+  //   this.create = function () {
+  //     var _this = this;
 
-      this.activity.loader(true);
-      network["native"]('https://cors.apn.monster/https://www.radiorecord.ru/api/stations/', this.build.bind(this), function () {
-        var empty = new Lampa.Empty();
-        html.append(empty.render());
-        _this.start = empty.start;
+  //     this.activity.loader(true);
+  //     network["native"]('https://cors.apn.monster/https://www.radiorecord.ru/api/stations/', this.build.bind(this), function () {
+  //       var empty = new Lampa.Empty();
+  //       html.append(empty.render());
+  //       _this.start = empty.start;
 
-        _this.activity.loader(false);
+  //       _this.activity.loader(false);
 
-        _this.activity.toggle();
-      });
-      Lampa.Background.immediately('');
-      return this.render();
-    };
+  //       _this.activity.toggle();
+  //     });
+  //     Lampa.Background.immediately('');
+  //     return this.render();
+  //   };
 
-    this.build = function (data) {
-      var _this2 = this;
+  //   this.build = function (data) {
+  //     var _this2 = this;
 
-      scroll.minus();
-      html.append(scroll.render());
-      data.result.genre.forEach(function (element) {
-        var results = data.result.stations.filter(function (station) {
-          return station.genre.filter(function (genre) {
-            return genre.id == element.id;
-          }).length;
-        });
+  //     scroll.minus();
+  //     html.append(scroll.render());
+  //     data.result.genre.forEach(function (element) {
+  //       var results = data.result.stations.filter(function (station) {
+  //         return station.genre.filter(function (genre) {
+  //           return genre.id == element.id;
+  //         }).length;
+  //       });
 
-        _this2.append({
-          title: element.name,
-          results: results
-        });
-      });
-      this.activity.loader(false);
-      this.activity.toggle();
-    };
+  //       _this2.append({
+  //         title: element.name,
+  //         results: results
+  //       });
+  //     });
+  //     this.activity.loader(false);
+  //     this.activity.toggle();
+  //   };
 
-    this.append = function (element) {
-      var item = new create(element);
-      item.create();
-      item.onDown = this.down.bind(this);
-      item.onUp = this.up.bind(this);
-      item.onBack = this.back.bind(this);
-      scroll.append(item.render());
-      items.push(item);
-    };
+  //   this.append = function (element) {
+  //     var item = new create(element);
+  //     item.create();
+  //     item.onDown = this.down.bind(this);
+  //     item.onUp = this.up.bind(this);
+  //     item.onBack = this.back.bind(this);
+  //     scroll.append(item.render());
+  //     items.push(item);
+  //   };
 
-    this.back = function () {
-      Lampa.Activity.backward();
-    };
+  //   this.back = function () {
+  //     Lampa.Activity.backward();
+  //   };
 
-    this.down = function () {
-      active++;
-      active = Math.min(active, items.length - 1);
-      items[active].toggle();
-      scroll.update(items[active].render());
-    };
+  //   this.down = function () {
+  //     active++;
+  //     active = Math.min(active, items.length - 1);
+  //     items[active].toggle();
+  //     scroll.update(items[active].render());
+  //   };
 
-    this.up = function () {
-      active--;
+  //   this.up = function () {
+  //     active--;
 
-      if (active < 0) {
-        active = 0;
-        Lampa.Controller.toggle('head');
-      } else {
-        items[active].toggle();
-      }
+  //     if (active < 0) {
+  //       active = 0;
+  //       Lampa.Controller.toggle('head');
+  //     } else {
+  //       items[active].toggle();
+  //     }
 
-      scroll.update(items[active].render());
-    };
+  //     scroll.update(items[active].render());
+  //   };
 
-    this.start = function () {
-      Lampa.Controller.add('content', {
-        toggle: function toggle() {
-          if (items.length) {
-            items[active].toggle();
-          }
-        },
-        back: this.back
-      });
-      Lampa.Controller.toggle('content');
-    };
+  //   this.start = function () {
+  //     Lampa.Controller.add('content', {
+  //       toggle: function toggle() {
+  //         if (items.length) {
+  //           items[active].toggle();
+  //         }
+  //       },
+  //       back: this.back
+  //     });
+  //     Lampa.Controller.toggle('content');
+  //   };
 
-    this.pause = function () { };
+  //   this.pause = function () { };
 
-    this.stop = function () { };
+  //   this.stop = function () { };
 
-    this.render = function () {
-      return html;
-    };
+  //   this.render = function () {
+  //     return html;
+  //   };
 
-    this.destroy = function () {
-      network.clear();
-      Lampa.Arrays.destroy(items);
-      scroll.destroy();
-      html.remove();
-      items = null;
-      network = null;
-    };
-  }
+  //   this.destroy = function () {
+  //     network.clear();
+  //     Lampa.Arrays.destroy(items);
+  //     scroll.destroy();
+  //     html.remove();
+  //     items = null;
+  //     network = null;
+  //   };
+  // }
 
-  function player() {
-    var html = Lampa.Template.get('radio_player', {});
-    var audio = new Audio();
-    var url = '';
-    var played = false;
-    var hls;
-    audio.addEventListener("play", function (event) {
-      played = true;
-      html.toggleClass('loading', false);
-    });
+  // function player() {
+  //   var html = Lampa.Template.get('radio_player', {});
+  //   var audio = new Audio();
+  //   var url = '';
+  //   var played = false;
+  //   var hls;
+  //   audio.addEventListener("play", function (event) {
+  //     played = true;
+  //     html.toggleClass('loading', false);
+  //   });
 
-    function prepare() {
-      if (audio.canPlayType('application/vnd.apple.mpegurl') || url.indexOf('.aacp') > 0) load(); else if (Hls.isSupported()) {
-        try {
-          hls = new Hls();
-          hls.attachMedia(audio);
-          hls.loadSource(url);
-          hls.on(Hls.Events.ERROR, function (event, data) {
-            if (data.details === Hls.ErrorDetails.MANIFEST_PARSING_ERROR) {
-              if (data.reason === "no EXTM3U delimiter") {
-                Lampa.Noty.show('Ошибка в загрузке потока');
-              }
-            }
-          });
-          hls.on(Hls.Events.MANIFEST_LOADED, function () {
-            start();
-          });
-        } catch (e) {
-          Lampa.Noty.show('Ошибка в загрузке потока');
-        }
-      } else load();
-    }
+  //   function prepare() {
+  //     if (audio.canPlayType('application/vnd.apple.mpegurl') || url.indexOf('.aacp') > 0) load(); else if (Hls.isSupported()) {
+  //       try {
+  //         hls = new Hls();
+  //         hls.attachMedia(audio);
+  //         hls.loadSource(url);
+  //         hls.on(Hls.Events.ERROR, function (event, data) {
+  //           if (data.details === Hls.ErrorDetails.MANIFEST_PARSING_ERROR) {
+  //             if (data.reason === "no EXTM3U delimiter") {
+  //               Lampa.Noty.show('Ошибка в загрузке потока');
+  //             }
+  //           }
+  //         });
+  //         hls.on(Hls.Events.MANIFEST_LOADED, function () {
+  //           start();
+  //         });
+  //       } catch (e) {
+  //         Lampa.Noty.show('Ошибка в загрузке потока');
+  //       }
+  //     } else load();
+  //   }
 
-    function load() {
-      audio.src = url;
-      audio.load();
-      start();
-    }
+  //   function load() {
+  //     audio.src = url;
+  //     audio.load();
+  //     start();
+  //   }
 
-    function start() {
-      var playPromise;
+  //   function start() {
+  //     var playPromise;
 
-      try {
-        playPromise = audio.play();
-      } catch (e) { }
+  //     try {
+  //       playPromise = audio.play();
+  //     } catch (e) { }
 
-      if (playPromise !== undefined) {
-        playPromise.then(function () {
-          console.log('Radio', 'start plaining');
-        })["catch"](function (e) {
-          console.log('Radio', 'play promise error:', e.message);
-        });
-      }
-    }
+  //     if (playPromise !== undefined) {
+  //       playPromise.then(function () {
+  //         console.log('Radio', 'start plaining');
+  //       })["catch"](function (e) {
+  //         console.log('Radio', 'play promise error:', e.message);
+  //       });
+  //     }
+  //   }
 
-    function play() {
-      html.toggleClass('loading', true);
-      html.toggleClass('stop', false);
-      prepare();
-    }
+  //   function play() {
+  //     html.toggleClass('loading', true);
+  //     html.toggleClass('stop', false);
+  //     prepare();
+  //   }
 
-    function stop() {
-      played = false;
-      html.toggleClass('stop', true);
-      html.toggleClass('loading', false);
+  //   function stop() {
+  //     played = false;
+  //     html.toggleClass('stop', true);
+  //     html.toggleClass('loading', false);
 
-      if (hls) {
-        hls.destroy();
-        hls = false;
-      }
+  //     if (hls) {
+  //       hls.destroy();
+  //       hls = false;
+  //     }
 
-      audio.src = '';
-    }
+  //     audio.src = '';
+  //   }
 
-    html.on('hover:enter', function () {
-      if (played) stop(); else if (url) play();
-    });
+  //   html.on('hover:enter', function () {
+  //     if (played) stop(); else if (url) play();
+  //   });
 
-    this.create = function () {
-      $('.head__actions .open--search').before(html);
-    };
+  //   this.create = function () {
+  //     $('.head__actions .open--search').before(html);
+  //   };
 
-    this.play = function (data) {
-      stop();
-      url = (country == 'UA' ? 'https://apn.watch/' : '') + (data.stream_320 ? data.stream_320 : data.stream_128 ? data.stream_128 : data.stream_hls.replace('playlist.m3u8', '96/playlist.m3u8'));
-      html.find('.radio-player__name').text(data.title);
-      html.toggleClass('hide', false);
-      play();
-    };
+  //   this.play = function (data) {
+  //     stop();
+  //     url = (country == 'UA' ? 'https://apn.watch/' : '') + (data.stream_320 ? data.stream_320 : data.stream_128 ? data.stream_128 : data.stream_hls.replace('playlist.m3u8', '96/playlist.m3u8'));
+  //     html.find('.radio-player__name').text(data.title);
+  //     html.toggleClass('hide', false);
+  //     play();
+  //   };
   }
 
   function startPlugin() {
     window.radio = true;
     Lampa.Component.add('radio', component);
-    // Lampa.Template.add('radio_item', "<div class=\"selector radio-item\">\n        <div class=\"radio-item__imgbox\">\n            <img class=\"radio-item__img\" />\n        </div>\n\n        <div class=\"radio-item__name\">{name}</div>\n    </div>");
+    Lampa.Template.add('radio_item', "<div class=\"selector radio-item\">\n        <div class=\"radio-item__imgbox\">\n            <img class=\"radio-item__img\" />\n        </div>\n\n        <div class=\"radio-item__name\">{name}</div>\n    </div>");
     // Lampa.Template.add('radio_player', "<div class=\"selector radio-player stop hide\">\n        <div class=\"radio-player__name\">Radio Record</div>\n\n        <div class=\"radio-player__button\">\n            <i></i>\n            <i></i>\n            <i></i>\n            <i></i>\n        </div>\n    </div>");
     // Lampa.Template.add('radio_style', "<style>\n    .radio-item {\n        width: 8em;\n        -webkit-flex-shrink: 0;\n            -ms-flex-negative: 0;\n                flex-shrink: 0;\n      }\n      .radio-item__imgbox {\n        background-color: #3E3E3E;\n        padding-bottom: 83%;\n        position: relative;\n        -webkit-border-radius: 0.3em;\n           -moz-border-radius: 0.3em;\n                border-radius: 0.3em;\n      }\n      .radio-item__img {\n        position: absolute;\n        top: 0;\n        left: 0;\n        width: 100%;\n        height: 100%;\n      }\n      .radio-item__name {\n        font-size: 1.1em;\n        margin-top: 0.8em;\n      }\n      .radio-item.focus .radio-item__imgbox:after {\n        border: solid 0.4em #fff;\n        content: \"\";\n        display: block;\n        position: absolute;\n        left: 0;\n        top: 0;\n        right: 0;\n        bottom: 0;\n        -webkit-border-radius: 0.3em;\n           -moz-border-radius: 0.3em;\n                border-radius: 0.3em;\n      }\n      .radio-item + .radio-item {\n        margin-left: 1em;\n      }\n      \n      @-webkit-keyframes sound {\n        0% {\n          height: 0.1em;\n        }\n        100% {\n          height: 1em;\n        }\n      }\n      \n      @-moz-keyframes sound {\n        0% {\n          height: 0.1em;\n        }\n        100% {\n          height: 1em;\n        }\n      }\n      \n      @-o-keyframes sound {\n        0% {\n          height: 0.1em;\n        }\n        100% {\n          height: 1em;\n        }\n      }\n      \n      @keyframes sound {\n        0% {\n          height: 0.1em;\n        }\n        100% {\n          height: 1em;\n        }\n      }\n      @-webkit-keyframes sound-loading {\n        0% {\n          -webkit-transform: rotate(0deg);\n                  transform: rotate(0deg);\n        }\n        100% {\n          -webkit-transform: rotate(360deg);\n                  transform: rotate(360deg);\n        }\n      }\n      @-moz-keyframes sound-loading {\n        0% {\n          -moz-transform: rotate(0deg);\n               transform: rotate(0deg);\n        }\n        100% {\n          -moz-transform: rotate(360deg);\n               transform: rotate(360deg);\n        }\n      }\n      @-o-keyframes sound-loading {\n        0% {\n          -o-transform: rotate(0deg);\n             transform: rotate(0deg);\n        }\n        100% {\n          -o-transform: rotate(360deg);\n             transform: rotate(360deg);\n        }\n      }\n      @keyframes sound-loading {\n        0% {\n          -webkit-transform: rotate(0deg);\n             -moz-transform: rotate(0deg);\n               -o-transform: rotate(0deg);\n                  transform: rotate(0deg);\n        }\n        100% {\n          -webkit-transform: rotate(360deg);\n             -moz-transform: rotate(360deg);\n               -o-transform: rotate(360deg);\n                  transform: rotate(360deg);\n        }\n      }\n      .radio-player {\n        display: -webkit-box;\n        display: -webkit-flex;\n        display: -moz-box;\n        display: -ms-flexbox;\n        display: flex;\n        -webkit-box-align: center;\n        -webkit-align-items: center;\n           -moz-box-align: center;\n            -ms-flex-align: center;\n                align-items: center;\n        -webkit-border-radius: 0.3em;\n           -moz-border-radius: 0.3em;\n                border-radius: 0.3em;\n        padding: 0.2em 0.8em;\n        background-color: #3e3e3e;\n      }\n      .radio-player__name {\n        margin-right: 1em;\n        white-space: nowrap;\n        overflow: hidden;\n        -o-text-overflow: ellipsis;\n           text-overflow: ellipsis;\n        max-width: 8em;\n      }\n      @media screen and (max-width: 385px) {\n        .radio-player__name {\n          display: none;\n        }\n      }\n      .radio-player__button {\n        position: relative;\n        width: 1.5em;\n        height: 1.5em;\n        display: -webkit-box;\n        display: -webkit-flex;\n        display: -moz-box;\n        display: -ms-flexbox;\n        display: flex;\n        -webkit-box-align: center;\n        -webkit-align-items: center;\n           -moz-box-align: center;\n            -ms-flex-align: center;\n                align-items: center;\n        -webkit-box-pack: center;\n        -webkit-justify-content: center;\n           -moz-box-pack: center;\n            -ms-flex-pack: center;\n                justify-content: center;\n        -webkit-flex-shrink: 0;\n            -ms-flex-negative: 0;\n                flex-shrink: 0;\n      }\n      .radio-player__button i {\n        display: block;\n        width: 0.2em;\n        background-color: #fff;\n        margin: 0 0.1em;\n        -webkit-animation: sound 0ms -800ms linear infinite alternate;\n           -moz-animation: sound 0ms -800ms linear infinite alternate;\n             -o-animation: sound 0ms -800ms linear infinite alternate;\n                animation: sound 0ms -800ms linear infinite alternate;\n        -webkit-flex-shrink: 0;\n            -ms-flex-negative: 0;\n                flex-shrink: 0;\n      }\n      .radio-player__button i:nth-child(1) {\n        -webkit-animation-duration: 474ms;\n           -moz-animation-duration: 474ms;\n             -o-animation-duration: 474ms;\n                animation-duration: 474ms;\n      }\n      .radio-player__button i:nth-child(2) {\n        -webkit-animation-duration: 433ms;\n           -moz-animation-duration: 433ms;\n             -o-animation-duration: 433ms;\n                animation-duration: 433ms;\n      }\n      .radio-player__button i:nth-child(3) {\n        -webkit-animation-duration: 407ms;\n           -moz-animation-duration: 407ms;\n             -o-animation-duration: 407ms;\n                animation-duration: 407ms;\n      }\n      .radio-player__button i:nth-child(4) {\n        -webkit-animation-duration: 458ms;\n           -moz-animation-duration: 458ms;\n             -o-animation-duration: 458ms;\n                animation-duration: 458ms;\n      }\n      .radio-player.stop .radio-player__button {\n        -webkit-border-radius: 100%;\n           -moz-border-radius: 100%;\n                border-radius: 100%;\n        border: 0.2em solid #fff;\n      }\n      .radio-player.stop .radio-player__button i {\n        display: none;\n      }\n      .radio-player.stop .radio-player__button:after {\n        content: \"\";\n        width: 0.5em;\n        height: 0.5em;\n        background-color: #fff;\n      }\n      .radio-player.loading .radio-player__button:before {\n        content: \"\";\n        display: block;\n        border-top: 0.2em solid #fff;\n        border-left: 0.2em solid transparent;\n        border-right: 0.2em solid transparent;\n        border-bottom: 0.2em solid transparent;\n        -webkit-animation: sound-loading 1s linear infinite;\n           -moz-animation: sound-loading 1s linear infinite;\n             -o-animation: sound-loading 1s linear infinite;\n                animation: sound-loading 1s linear infinite;\n        width: 0.9em;\n        height: 0.9em;\n        -webkit-border-radius: 100%;\n           -moz-border-radius: 100%;\n                border-radius: 100%;\n        -webkit-flex-shrink: 0;\n            -ms-flex-negative: 0;\n                flex-shrink: 0;\n      }\n      .radio-player.loading .radio-player__button i {\n        display: none;\n      }\n      .radio-player.focus {\n        background-color: #fff;\n        color: #000;\n      }\n      .radio-player.focus .radio-player__button {\n        border-color: #000;\n      }\n      .radio-player.focus .radio-player__button i, .radio-player.focus .radio-player__button:after {\n        background-color: #000;\n      }\n      .radio-player.focus .radio-player__button:before {\n        border-top-color: #000;\n      }\n    </style>");
     // window.radio_player = new player();
