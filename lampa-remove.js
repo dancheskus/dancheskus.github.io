c33b260132c8c8da35668c1b22a5cf2125609a6a
Daniel Shleifman <dancheskus@gmail.com>
2024-04-20 17:06:53 +0300

    test

--- lampa-remove.js
+++ lampa-remove.js
@@ -7,6 +7,10 @@
         console.log(Lampa)
         $("[data-action=anime]").eq(0).remove();
         $("[data-action=timetable]").eq(0).remove();
+        $("[data-action=feed]").eq(0).remove();
+        $("[data-action=myperson]").eq(0).remove();
+        $("[data-action=subscribes]").eq(0).remove();
+        $("[data-action=about]").eq(0).remove();
       }, 10);
     }
   });
